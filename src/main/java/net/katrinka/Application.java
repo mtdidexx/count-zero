662e9dc990cdc7950651ad81ccdbf838c4d64c14
Mark Donahue <mark-donahue@idexx.com>
2017-07-18 19:58:06 -0400

    setup

---
+++ src/main/java/net/katrinka/Application.java
@@ -0,0 +1,13 @@
+package net.katrinka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ApplicationContext;
+
+/**
+ * Created by mdonahue on 7/18/17.
+ */
+public class Application {
+    public static void main(String[] args) {
+        ApplicationContext ctx = SpringApplication.run(Application.class, args);
+    }
+}
