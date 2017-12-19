622c17d3f5cbb4cede12d14da39f47d394c48261
Mark Donahue <mark-donahue@idexx.com>
2017-12-19 11:13:36 -0500

    Add a test that will fail to test notifications

---
+++ src/test/groovy/net/katrinka/services/NotReallyATest.groovy
@@ -0,0 +1,10 @@
+package net.katrinka.services
+
+import spock.lang.Specification
+
+class NotReallyATest extends Specification {
+    def "it will just fail"() {
+        expect:
+        true == false
+    }
+}
